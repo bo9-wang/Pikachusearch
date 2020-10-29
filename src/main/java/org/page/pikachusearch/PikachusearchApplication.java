d26a1019fd5202e09c23d0b9808d171f4cc3be43
Bo9_Wang <wangbo90221@outlook.com>
2020-10-29 10:49:05 +0800

    初始化

---
+++ src/main/java/org/page/pikachusearch/PikachusearchApplication.java
@@ -0,0 +1,13 @@
+package org.page.pikachusearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikachusearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PikachusearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/page/pikachusearch/PikachusearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.page.pikachusearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikachusearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
