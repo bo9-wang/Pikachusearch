e1aef9fc03e9f5577261083f1d1d258b5d7be4f2
Bo9_Wang <wangbo90221@outlook.com>
2020-10-30 10:22:46 +0800

    String 测试

--- src/test/java/org/page/pikachusearch/PikachusearchApplicationTests.java
+++ src/test/java/org/page/pikachusearch/PikachusearchApplicationTests.java
@@ -6,8 +6,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PikachusearchApplicationTests {
 
+
+
     @Test
     void contextLoads() {
+        String str1 = "11";
+        String str2 = "22";
+        stringTest(str1,str2);
+        System.out.println(str1+",,"+str2);
+    }
+
+    void stringTest(String str1,String str2){
+        str1 = "111";
+        str2 = "222";
+
     }
 
 }
